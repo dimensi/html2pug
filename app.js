18d6a302ec9345e96536e3ff47f17a629a2f5093
Nikita Nafranets <eddimensi@gmail.com>
2016-09-22 02:44:42 +0300

    Изменил порт

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.get('/', function(req, res) {
     res.render('index', { title: 'html2pug'});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Сервер запущен');
 });
